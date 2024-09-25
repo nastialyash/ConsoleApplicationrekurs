6d6b6e96ad11c0040158aeb9f47034fe818c2f3f
nastialyash <user251@school22.rv.ua>
2024-09-25 21:53:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplicationrekurs.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+    int num(int a, int b) {
+        if (b == 0)
+            return a;
+        return num(b, a % b);
+    }
+
+    int main() {
+        int num1, num2;
+
+        cout << "Enter 2 numbers :  ";
+        cin >> num1 >> num2;
+
+        cout << "REsult :  " << num1 << " і " << num2 << " = " << num(num1, num2) << endl;
+
+
+
+
+}
+
